bb382a4757410935763dff52dbfa88c0129fa5a2
Mark Harfouche <mark.harfouche@gmail.com>
2019-01-06 11:07:02 -0500

    Add --host=${HOST} to build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --host=${HOST}
 # bootstrap make with their own build script
 sh build.sh 
 # make check depends on lots of locales
-# make check
+./make check
 ./make install
