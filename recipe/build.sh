aa742fb8201609309aec2091e692f38423a0a5fe
Mark Harfouche <mark.harfouche@gmail.com>
2019-01-06 12:51:08 -0500

    echeo the commands

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
+set -ex
 ./configure --prefix=$PREFIX --host=${HOST}
 # bootstrap make with their own build script
-sh build.sh 
+sh build.sh
 # make check depends on lots of locales
 ./make check
 ./make install
