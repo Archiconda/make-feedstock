7c9460f32d51cdda1c5486b3dc230c05b67e5f4a
Mark Harfouche <mark.harfouche@gmail.com>
2019-01-08 13:44:38 -0500

    specify build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 set -ex
-./configure --prefix=$PREFIX --host=${HOST}
+./configure --prefix=$PREFIX --host=${HOST} --build=${BUILD}
 # bootstrap make with their own build script
 sh build.sh
 # make check depends on lots of locales
